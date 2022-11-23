8606a334c83fcf45f6a4ccab290b8daff7a03eaf
Giljinsu <therin234@gmail.com>
2022-11-23 14:32:53 +0900

    1commit

---
+++ docs/browser_js/js/events.js
@@ -0,0 +1,18 @@
+function changeText() {
+    let queryChangeText = document.querySelector('#changeText');
+    queryChangeText.innerHTML = "Change on me";
+}
+
+let queryClickalert = document.querySelector('#clickalert');
+// element.addEventListener(event, function, useCapture)
+queryClickalert.addEventListener("click",clickAlert);
+function clickAlert() {
+    alert("click on me!");
+}
+
+let queryButton = document.querySelector('#buttonChangeText');
+queryButton.addEventListener("click",targetText);
+function targetText(event) {
+    let queryTargetText = document.querySelector('#targetText');
+    queryTargetText.innerHTML = "Target Text!";
+}
